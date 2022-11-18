d37e962147f5a15486a6747eea14355b4dcc747f
Juan I. Campos R <jicampos@devmobile.cl>
2022-11-18 12:09:02 -0300

    Upd Cotizador

--- cotizador_l10n_cl/__manifest__.py
+++ cotizador_l10n_cl/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Cotizador de Productos',
-    'version': '15.0.1.10',
+    'version': '15.0.1.11',
     'category': 'Sales',
     'summary': 'Cotizador de Productos',
     'description': """
