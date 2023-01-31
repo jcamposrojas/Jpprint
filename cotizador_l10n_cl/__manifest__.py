6f04df50c4a1255ea33bcc662f166bdcc0e93cc9
Juan I. Campos R <jicampos@devmobile.cl>
2023-01-31 10:27:11 -0300

    Upd cotizador v33

--- cotizador_l10n_cl/__manifest__.py
+++ cotizador_l10n_cl/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Cotizador de Productos',
-    'version': '15.0.1.32',
+    'version': '15.0.1.33',
     'category': 'Sales',
     'summary': 'Cotizador de Productos',
     'description': """
