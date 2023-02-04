b38f3602315d0e66c658970a1448f39c44f565f0
Juan I. Campos R <jicampos@devmobile.cl>
2023-02-03 21:24:19 -0300

    Upd cotizador v40

--- cotizador_l10n_cl/models/tabla_troquel.py
+++ cotizador_l10n_cl/models/tabla_troquel.py
@@ -35,43 +35,14 @@ class TablaTroquel(models.Model):
     tipo_troquel = fields.Selection(TIPO_TROQUEL,string='tipo_troquel')
     etiquetas_al_ancho      = fields.Integer(string='Etiquetas al Ancho')
     gap                     = fields.Float(string='Gap', compute='_compute_gap', digits=(10, 3))
-    gap_minimo              = fields.Float(string='Gap Mímino', default=1.0)
+    gap_minimo              = fields.Float(string='Gap Mímino', default=3)
     etiquetas_al_desarrollo = fields.Integer(string='Etiquetas al Desarrollo', compute='_compute_gap')
 
-#    @api.depends('gap')
-#    def _compute_et_al_desarrollo(self):
-#        for rec in self:
-#            if rec.gap > 0:
-#                perimetro = round(rec.z * 3.175,3)
-#                l = round(perimetro / (rec.largo + rec.gap),0)
-#                rec.etiquetas_al_desarrollo = l
-#            else:
-#                rec.etiquetas_al_desarrollo = 0
 
-    @api.depends('largo', 'ancho')
+    @api.depends('largo', 'ancho','z','etiquetas_al_ancho')
     def _compute_name(self):
         for rec in self:
             rec.name = "%3s X %3s (Z=%s,S=%s)" % (rec.largo,rec.ancho,rec.z,rec.etiquetas_al_ancho)
-
-#    @api.depends('z', 'largo', 'gap_minimo')
-#    def _compute_gap(self):
-#        for rec in self:
-#            if rec.z > 0 and rec.largo > 0:
-#                i = 1
-#                c_calc = 0
-#                c_calc_ant = 0
-#                # Muy importante el redondeo!!!
-#                perimetro = round(rec.z * 3.175,3)
-#                while True:
-#                    c_calc_ant = c_calc
-#                    c_calc = perimetro / i  - rec.largo
-#
-#                    if c_calc < rec.gap_minimo:
-#                        break
-#                    i = i + 1
-#                rec.gap = perimetro / (i-1) - rec.largo
-#            else:
-#                rec.gap = 0
 
     @api.depends('z', 'largo', 'gap_minimo')
     def _compute_gap(self):
